66e2dd167fa0788f464440d2062968fe2e064788
Calvin <calvinchui382025@yahoo.com>
2023-02-06 10:24:52 -0600

    max width container and initial footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,1 +1,33 @@
-export default {};
+import { styled } from '@mui/material';
+import { useInView } from 'react-intersection-observer';
+import { animateProps, moveUpAnimation } from '../../utils/constants';
+//======================================================
+const FooterRoot = styled('div')<animateProps>(({animate}) => ({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // height: '100%',
+    // maxHeight: '300px',
+    backgroundColor: 'black',
+    color: 'white',
+    // fontSize: '1.5rem',
+    // fontWeight: 'bold',
+    // fontFamily: 'Roboto',
+    // textTransform: 'uppercase',
+    // letterSpacing: '0.1rem',
+    // animation: animate ? `${moveUpAnimation} 1s` : 'none',
+    width: '100%'
+  })
+)
+//======================================================
+const Footer = () => {
+  const { ref: footerRef, inView: footerInView } = useInView();
+
+  return (
+    <FooterRoot ref={footerRef} animate={footerInView}>
+      <p>Footer</p>
+    </FooterRoot>
+  )
+}
+//======================================================
+export default Footer;
