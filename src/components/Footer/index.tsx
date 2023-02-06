61601389a09866e3e0773afc48b4ac5e431fe91b
Calvin <calvinchui382025@yahoo.com>
2023-02-06 10:45:01 -0600

    footer image and scaling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,11 +4,12 @@ import { animateProps, moveUpAnimation } from '../../utils/constants';
 //======================================================
 const FooterRoot = styled('div')<animateProps>(({animate}) => ({
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     // height: '100%',
     // maxHeight: '300px',
-    backgroundColor: 'black',
+    backgroundColor: '#242524',
     color: 'white',
     // fontSize: '1.5rem',
     // fontWeight: 'bold',
@@ -19,12 +20,29 @@ const FooterRoot = styled('div')<animateProps>(({animate}) => ({
     width: '100%'
   })
 )
+const FooterImageContainer = styled('div')({
+  backgroundColor: '#181819',
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  
+})
+const FooterImage = styled('img')({
+  maxWidth: '600px',
+});
 //======================================================
 const Footer = () => {
   const { ref: footerRef, inView: footerInView } = useInView();
 
   return (
     <FooterRoot ref={footerRef} animate={footerInView}>
+      <FooterImageContainer>
+        <FooterImage
+          src="/images/footer-saying.jpg" 
+          alt="footer" 
+          width="100%"
+        />
+      </FooterImageContainer>
       <p>Footer</p>
     </FooterRoot>
   )
