ac5f17a967262fddc753a6725da39dd3a2b049c7
Lucas Overbey <lucasaoverbey@gmail.com>
2023-02-06 13:20:34 -0600

    added rowcontainer component, updated profilecards

--- src/Pages/Liability/index.tsx
+++ src/Pages/Liability/index.tsx
@@ -1,10 +1,22 @@
 import { Root } from '../../styled-components'
 import { styled, Card } from '@mui/material'
 
+const IntroInformationArea = styled('div')({
+  position: "absolute",
+  top: 0,
+
+})
+
 export const Liability = () => {
   return (
     <Root>
-      test
+      <IntroInformationArea>
+        <img
+        src="/images/liabilitytop.jpg"
+        style={{height: '271.17px'}}
+        >
+        </img>
+      </IntroInformationArea>
     </Root>
   )
 }
