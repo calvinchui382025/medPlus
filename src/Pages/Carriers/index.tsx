314d25b3abf92963169b85760022b240280ab902
Calvin <calvinchui382025@yahoo.com>
2023-02-06 14:07:41 -0600

    extracted carrier styled components

--- src/Pages/Carriers/index.tsx
+++ src/Pages/Carriers/index.tsx
@@ -1,115 +1,14 @@
-import { Divider, List, ListItem, ListItemButton, Typography, Card, Collapse } from '@mui/material'
-import { styled } from '@mui/system'
+import { ListItem, Typography, Collapse } from '@mui/material'
+
 import { useState } from 'react'
-import { mainColor } from '../../App'
-import { ContentContainer, Root } from '../../styled-components'
-import { CarrierRatingColorObj, CarriersList, niceBoxShadow } from '../../utils/constants'
+import { Root } from '../../styled-components'
+import { CarrierRatingColorObj, CarriersList } from '../../utils/constants'
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import Footer from '../../components/Footer'
-import ListItemText from '@mui/material/ListItemText'
+import { CarrierContentContainer, CarrierTextContainer, CarrierPageText, InfoContainer, StyledListContainer, CarrierList, CarrierListItem, CarrierListButton, CarrierName, CarrierDetailsWrapper, CarrierDescription, CarrierRating, RatingInfoContainer, RatingList, RatingListItemTextLeft, leftTextProps, RatingListItemTextRight, rightTextProps } from './styledComponents'
 //==============================================================================
-const CarrierContentContainer = styled(ContentContainer)(({theme}) => ({
-}));
-const CarrierTextContainer = styled('div')(({theme}) => ({
-  display: 'flex',
-  flexDirection: 'row',
-  width: '100%',
-  padding: '24px',
-}));
-const CarrierPageText = styled(Typography)(({theme}) => ({
-}))
-const InfoContainer = styled('div')(({theme}) => ({
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-around',
-  width: '100%',
-  '@media (max-width: 768px)': {
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-}));
-const RatingInfoContainer = styled('div')(({theme}) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  width: '33%',
-  minWidth: '350px',
-}));
-const RatingList = styled(List)(({theme}) => ({
-  width: '100%',
-}));
-const leftTextProps = {
-  fontSize: '0.8rem',
-  fontWeight: 'bold'
-}
-const rightTextProps = {
-  // textAlign: 'right',
-  fontSize: '0.8rem',
-}
-const RatingListItemTextLeft = styled(ListItemText)(({theme}) => ({
-  display: 'flex',
-  justifyContent: 'flex-start',
-}));
 
-const RatingListItemTextRight = styled(ListItemText)(({theme}) => ({
-  display: 'flex',
-  justifyContent: 'flex-end',
-  textAlign: 'right',
-}));
-
-const StyledListContainer = styled('div')(({theme}) => (
-  {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    width: '33%',
-    minWidth: '350px',
-  })
-)
-const CarrierList = styled(List)(({theme}) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-  width: '100%',
-}))
-const CarrierListItem = styled(ListItem)(({theme}) => ({
-  width: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-  padding: 0,
-  margin: 0,
-}))
-const CarrierListButton = styled(ListItemButton)(({theme}) => ({
-  display: 'flex',
-  flexDirection: 'row',
-  width: '100%',
-  justifyContent: 'space-between',
-}))
-
-const CarrierName = styled(Typography)(({theme}) => ({
-    color: mainColor,
-    fontWeight: 'bold',
-    fontSize: '1rem',
-  })
-)
-const CarrierDetailsWrapper = styled('div')({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-  width: '100%',
-  padding: '12px 32px',
-
-})
-const CarrierDescription = styled(Typography)(({theme}) => ({
-    
-  })
-)
-const CarrierRating = styled(Typography)(({theme}) => ({
-    fontWeight: 'bold',
-  })
-)
 //==============================================================================
 export const Carriers = () => {
 
