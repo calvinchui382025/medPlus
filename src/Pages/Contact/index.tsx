e5d448fce327d4917778b611988acaf74bc48ced
Calvin <calvinchui382025@yahoo.com>
2023-02-06 14:46:14 -0600

    Updates

--- src/Pages/Contact/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Root } from '../../styled-components'
-
-export const Contact = () => {
-  return (
-    <Root>Contact</Root>
-  )
-}
--- src/Pages/Quote/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Root } from '../../styled-components'
-
-export const Quote = () => {
-  return (
-    <Root>Quote</Root>
-  )
-}
